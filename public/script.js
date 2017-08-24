028f0ad8fcb693a6580c050048e0339866e29781
John T Cronin <jt.cronin@outlook.com>
2017-08-24 09:51:16 -0500

    trying to fix the click alert

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ window.addEventListener('click', function(){
 	var picture = document.getElementsByClassName('pic')[0];
 	picture.addEventListener('click', function(){
 		
-		alert(event.clientX + "," + event.clientY);
+		window.alert(event.clientX + "," + event.clientY);
 
 	});
 
